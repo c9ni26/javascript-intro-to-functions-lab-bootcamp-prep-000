686343b73bbb1adc5839ec951b1bf6b4469b5e0f
cody rivas <services4cody@protonmail.com>
2018-01-31 21:06:37 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -2,7 +2,3 @@ function shout(string){
   return string}
   'Hello!'.toUpperCase()
   function shout(string) { return string.toUpperCase()}
-function shout(string){return string}
-'Hello!'.toLowerCase()
-  function shour(string) { return string.toLowerCase()}
-  
